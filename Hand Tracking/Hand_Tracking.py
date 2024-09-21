d009695bd5c20f69189d1173cc336a85ec654bd8
dhn06038 <dhn06038@cau.ac.kr>
2024-09-21 17:55:38 +0900

    Get hand's landmark data

--- Hand Tracking/Hand_Tracking.py
+++ Hand Tracking/Hand_Tracking.py
@@ -1,19 +1,43 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+import socket
 
-# WebCam 을 사용할 경우
+# Parameters
+width, height = 1280, 720
+
+# WebCam's IP
 cap = cv2.VideoCapture(0)
 
-# 손을 감지
+cap.set(3, width)
+cap.set(4, height)
+
+# Detect two Hands
 detector = HandDetector(maxHands=1, detectionCon=0.8)
 
-# 웹켐에서 프레임 가져오기
+# Network
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+serverAddressPort = ("127.0.0.1", 5052)
+
+# Get Frame
 while True:
     success, img = cap.read()
 
 # Hands
     hands, img = detector.findHands(img)
 
+    data = []
+    # has 21 landmark data
+    if hands:
+        hand = hands[0]
+        lmList = hand['lmList']
+        print(lmList)
+
+        for lm in lmList:
+            data.extend([lm[0], height - lm[1], lm[2]])
+        print(data)
+        sock.sendto(str.encode(str(data)), serverAddressPort)
+
+    img = cv2.resize(img, (0, 0), None, 0.5, 0.5)
     cv2.imshow("Image", img)
 
     if cv2.waitKey(1) == ord("q"): # q 누를 시 웹켐 종료
