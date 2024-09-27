838b6037f8155e718af09158de711234710c768f
dhn06038 <dhn06038@cau.ac.kr>
2024-09-27 13:09:19 +0900

    Now we can track hands, face ,and pose at once

--- MoCap_Python/All_Tracking.py
+++ MoCap_Python/All_Tracking.py
@@ -21,6 +21,7 @@ faceDetector = FaceMeshDetector()
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 serverAddressPort1 = ("127.0.0.1", 5052)
 serverAddressPort2 = ("127.0.0.1", 5053)
+serverAddressPort3 = ("127.0.0.1", 5054)
 
 staticMode=False
 maxFaces=2
@@ -34,17 +35,18 @@ smoothSegmentation=True
 detectionCon=0.5
 trackCon=0.5
 
+maxHands=2
+
 mpDraw = mp.solutions.drawing_utils
+drawSpec = mpDraw.DrawingSpec(thickness=1, circle_radius=2)
 
 mpFaceMesh = mp.solutions.face_mesh
 faceMesh = mpFaceMesh.FaceMesh(static_image_mode=staticMode,
                                                  max_num_faces=maxFaces,
                                                  min_detection_confidence=minDetectionCon,
                                                  min_tracking_confidence=minTrackCon)
-drawSpec = mpDraw.DrawingSpec(thickness=1, circle_radius=2)
 
 mpPose = mp.solutions.pose
-
 pose = mpPose.Pose(static_image_mode=staticMode,
                                      model_complexity=modelComplexity,
                                      smooth_landmarks=smoothLandmarks,
@@ -52,17 +54,24 @@ pose = mpPose.Pose(static_image_mode=staticMode,
                                      smooth_segmentation=smoothSegmentation,
                                      min_detection_confidence=detectionCon,
                                      min_tracking_confidence=trackCon)
-posData = []
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(static_image_mode=staticMode,
+                                        max_num_hands=maxHands,
+                                        model_complexity=modelComplexity,
+                                        min_detection_confidence=detectionCon,
+                                        min_tracking_confidence=minTrackCon)
+
 # Get Frame
 while True:
     success, img = cap.read()
 
 # bbox
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    h, w, c = img.shape
 
     face_results = faceMesh.process(imgRGB)
     faces = []
-
     draw = True
 
     if face_results.multi_face_landmarks:
@@ -72,8 +81,7 @@ while True:
                                            drawSpec, drawSpec)
             face = []
             for id, lm in enumerate(faceLms.landmark):
-                ih, iw, ic = img.shape
-                x, y, z = int(lm.x * iw), int(lm.y * ih), (lm.z * iw)
+                x, y, z = int(lm.x * w), int(lm.y * h), (lm.z * w)
                 face.append([x, y, z])
             faces.append(face)
 
@@ -84,26 +92,25 @@ while True:
             mpDraw.draw_landmarks(img, pose_results.pose_landmarks,
                                        mpPose.POSE_CONNECTIONS)
 
-    lmList = []
+    pose_lmList = []
     bboxInfo = {}
     bboxWithHands = False
     if pose_results.pose_landmarks:
         for id, lm in enumerate(pose_results.pose_landmarks.landmark):
-            h, w, c = img.shape
             cx, cy, cz = int(lm.x * w), int(lm.y * h), int(lm.z * w)
-            lmList.append([cx, cy, cz])
+            pose_lmList.append([cx, cy, cz])
 
             # Bounding Box
-        ad = abs(lmList[12][0] - lmList[11][0]) // 2
+        ad = abs(pose_lmList[12][0] - pose_lmList[11][0]) // 2
         if bboxWithHands:
-            x1 = lmList[16][0] - ad
-            x2 = lmList[15][0] + ad
+            x1 = pose_lmList[16][0] - ad
+            x2 = pose_lmList[15][0] + ad
         else:
-            x1 = lmList[12][0] - ad
-            x2 = lmList[11][0] + ad
+            x1 = pose_lmList[12][0] - ad
+            x2 = pose_lmList[11][0] + ad
 
-        y2 = lmList[29][1] + ad
-        y1 = lmList[1][1] - ad
+        y2 = pose_lmList[29][1] + ad
+        y1 = pose_lmList[1][1] - ad
         bbox = (x1, y1, x2 - x1, y2 - y1)
         cx, cy = bbox[0] + (bbox[2] // 2), \
                     bbox[1] + bbox[3] // 2
@@ -114,12 +121,61 @@ while True:
             cv2.rectangle(img, bbox, (255, 0, 255), 3)
             cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
 
+    hands_results = hands.process(imgRGB)
+    flipType = True
+    allHands = []
+
+    if hands_results.multi_hand_landmarks:
+        for handType, handLms in zip(hands_results.multi_handedness, hands_results.multi_hand_landmarks):
+            myHand = {}
+            ## lmList
+            mylmList = []
+            xList = []
+            yList = []
+            for id, lm in enumerate(handLms.landmark):
+                px, py, pz = int(lm.x * w), int(lm.y * h), int(lm.z * w)
+                mylmList.append([px, py, pz])
+                xList.append(px)
+                yList.append(py)
+
+            ## bbox
+            xmin, xmax = min(xList), max(xList)
+            ymin, ymax = min(yList), max(yList)
+            boxW, boxH = xmax - xmin, ymax - ymin
+            hands_bbox = xmin, ymin, boxW, boxH
+            cx, cy = hands_bbox[0] + (hands_bbox[2] // 2), \
+                     hands_bbox[1] + (hands_bbox[3] // 2)
+
+            myHand["lmList"] = mylmList
+            myHand["bbox"] = hands_bbox
+            myHand["center"] = (cx, cy)
+
+            if flipType:
+                if handType.classification[0].label == "Right":
+                    myHand["type"] = "Left"
+                else:
+                    myHand["type"] = "Right"
+            else:
+                myHand["type"] = handType.classification[0].label
+            allHands.append(myHand)
+
+            ## draw
+            if draw:
+                mpDraw.draw_landmarks(img, handLms,
+                                           mpHands.HAND_CONNECTIONS)
+                cv2.rectangle(img, (hands_bbox[0] - 20, hands_bbox[1] - 20),
+                              (hands_bbox[0] + hands_bbox[2] + 20, hands_bbox[1] + hands_bbox[3] + 20),
+                              (255, 0, 255), 2)
+                cv2.putText(img, myHand["type"], (hands_bbox[0] - 30, hands_bbox[1] - 30), cv2.FONT_HERSHEY_PLAIN,
+                            2, (255, 0, 255), 2)
+
     pose_data = []
     face_data = []
+    hands_data = []
 
     # send landmark data
     if bboxInfo:
-        for lm in lmList:
+        for lm in pose_lmList:
             pose_data.extend([lm[0], height - lm[1], lm[2]])
         sock.sendto(str.encode(str(pose_data)), serverAddressPort1)
 
@@ -127,6 +183,14 @@ while True:
         for face in faces[0]:
             face_data.extend([face[0], height - face[1], face[2]])
         sock.sendto(str.encode(str(face_data)), serverAddressPort2)
+
+    if allHands:
+        hand = allHands[0]
+        hands_lmList = hand['lmList']
+
+        for lm in hands_lmList:
+            hands_data.extend([lm[0], height - lm[1], lm[2]])
+        sock.sendto(str.encode(str(hands_data)), serverAddressPort3)
 
     img = cv2.resize(img, (0, 0), None, 0.5, 0.5)
     cv2.imshow("Image", img)
